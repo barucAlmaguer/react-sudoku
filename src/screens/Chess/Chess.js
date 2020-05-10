513dc2c8081f6e2370fee1c54cc3e773ff4913a7
Baruc Almaguer <baruc.almaguer@gmail.com>
2020-05-10 16:40:20 -0500

    allow knight to move only in the expected manner (L-shape)

--- src/screens/Chess/Chess.js
+++ src/screens/Chess/Chess.js
@@ -5,8 +5,17 @@ import { Board } from './components'
 
 function Chess (props) {
   const [knightPos, setKnightPos] = useState([0, 0])
+  function canMoveKnight ([fromX, fromY], [toX, toY]) {
+    const dx = Math.abs(toX - fromX)
+    const dy = Math.abs(toY - fromY)
+    return ( dx === 1 && dy === 2 || dx === 2 && dy === 1 )
+  }
   function moveKnightAround ([ x, y ]) {
-    setKnightPos([x, y])
+    if (canMoveKnight([...knightPos], [x, y] )) {
+      setKnightPos([x, y])
+    } else {
+      console.warn('invalid knight move!')
+    }
   }
   return (
     <DndProvider backend={Backend}>
