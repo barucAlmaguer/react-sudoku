596fd78c49e2229512975964677ace20260809b0
Baruc Almaguer <baruc.almaguer@gmail.com>
2020-05-11 13:48:23 -0500

    Add Braids Playroom to project

---
+++ playroom.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  components: './src/components',
+  outputPath: './dist/playroom',
+
+  // Optional:
+  title: 'Test',
+  widths: [320, 375, 768, 1024],
+  port: 9000,
+  openBrowser: true,
+  exampleCode: `
+    <Button>
+      Hello World!
+    </Button>
+  `,
+  // title: 'My Awesome Library',
+  // themes: './src/themes',
+  // snippets: './playroom/snippets.js',
+  // frameComponent: './playroom/FrameComponent.js',
+  // widths: [320, 375, 768, 1024],
+  // port: 9000,
+  // openBrowser: true,
+  // exampleCode: `
+  //   <Button>
+  //     Hello World!
+  //   </Button>
+  // `,
+  // baseUrl: '/playroom/',
+  // webpackConfig: () => ({
+  //   // Custom webpack config goes here...
+  // })
+};
